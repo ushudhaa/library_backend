c14a8a7a9f01607f285e30f7b4a77a5d23c0ca60
ushudhaa <ushudhasanwa@gmail.com>
2025-08-31 12:19:15 +0545

    update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static("uploads/"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://library-frontend-beryl-sigma.vercel.app"],
     credentials: true,
   })
 );
